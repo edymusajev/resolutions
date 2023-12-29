8c2beb12653396e424e05d9da08b67338b0ab111
edymusajev <edymusajev@gmail.com>
2023-12-29 18:30:39 +0700

    Add new habits table and navigation buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,71 @@
 import Image from "next/image";
 
+const user = {
+  id: 1,
+  name: "John Doe",
+  habits: [1],
+};
+
+const habit = {
+  id: 1,
+  title: "go to the gym",
+  completed: [
+    {
+      date: "2021-01-01",
+    },
+    {
+      date: "2021-01-02",
+    },
+  ],
+};
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      lets go
+    <main className="p-24">
+      <nav className="flex space-x-2">
+        <button>previous</button>
+        <span>December</span>
+        <button>next</button>
+      </nav>
+
+      <table>
+        <thead>
+          {/* days in a week */}
+          <tr>
+            <th></th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+            <th>Sun</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Go to the gym</td>
+            <td>✓</td>
+            <td>x</td>
+            <td>✓</td>
+            <td>x</td>
+            <td>✓</td>
+            <td>x</td>
+            <td>x</td>
+          </tr>
+          <tr>
+            <td>Read a book</td>
+            <td>x</td>
+            <td>✓</td>
+            <td>x</td>
+            <td>✓</td>
+            <td>x</td>
+            <td>✓</td>
+            <td>x</td>
+          </tr>
+        </tbody>
+      </table>
+      <button>Add new habit</button>
     </main>
   );
 }
